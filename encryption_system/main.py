d95cc18c89fdfc8991359dcafec9487ef193c66e
unidentifiedfox <onur.kilic.b@gmail.com>
2025-12-01 04:30:38 +0300

    algorithm option update

--- encryption_system/main.py
+++ encryption_system/main.py
@@ -1,4 +1,4 @@
-import secrets, json, base64, socket, argparse
+import secrets, json, base64, socket, argparse, hashlib
 
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
@@ -10,19 +10,16 @@ from . import chacha20 as cha
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("type", help="Select Server or Client")
-    parser.add_argument("-m", "--message", help="Enter the path of the message")
+    parser.add_argument("type", choices=["server", "client"], help="Select server or client")
+    parser.add_argument("-m", "--message", help="Enter the path of the message file")
+    parser.add_argument("-a", "--algorithm", default="chacha20", choices=["chacha20", "rsa"],
+                        help="chacha20 by default, enter rsa to also encrypt the message with rsa")
     args = parser.parse_args()       
 
-    if args.type.lower() == "server":
+    if args.type == "server":
         server()
-        return
-    elif args.type.lower() == "client":
-        client(args.message)
-        return
-    else:
-        print("Invalid type")
-        return
+    elif args.type == "client":
+        client(args.message, args.algorithm)
 
 
 def server(host="127.0.0.1", port=5000):
@@ -46,27 +43,43 @@ def server(host="127.0.0.1", port=5000):
     while len(pdf_bytes) < size:
         pdf_bytes += conn.recv(size - len(pdf_bytes))
 
-    pdf_path = "./recieved_sample.pdf"
+    pdf_path = "./received_sample.pdf"
     with open(pdf_path, "wb") as f:
         f.write(pdf_bytes)
 
     extr_payload = extract_json(pdf_path)
+    extr_algorithm = extr_payload["algorithm"]
+    extr_signature = base64.b64decode(extr_payload["signature"])
     extr_encrypted_key_nonce =  base64.b64decode(extr_payload["encrypted_key_nonce"])
     extr_cipher = base64.b64decode(extr_payload["ciphertext"])
 
-    decrypted_key_nonce = rsa.decrypt(int.from_bytes(extr_encrypted_key_nonce, "big"),
-                                      server_keys["private_key"]).to_bytes(44, "big")
+    hash = hashlib.sha256(extr_cipher).digest()
+    signature = rsa.decrypt(int.from_bytes(extr_signature, "big"),
+                            client_pubkey).to_bytes(32, "big")
     
-    decrypted_nonce = decrypted_key_nonce[:12]
-    decrypted_key = decrypted_key_nonce[12:]
+    if hash != signature:
+        print("Signature mismatch")
+        return
 
-    print(cha.decrypt(decrypted_key, decrypted_nonce, extr_cipher).decode("utf-8"))
+    if extr_algorithm == "chacha20":
+        decrypted_key_nonce = rsa.decrypt(int.from_bytes(extr_encrypted_key_nonce, "big"),
+                                          server_keys["private_key"]).to_bytes(44, "big")
+    
+        decrypted_nonce = decrypted_key_nonce[:12]
+        decrypted_key = decrypted_key_nonce[12:]
+
+        print(cha.decrypt(decrypted_key, decrypted_nonce, extr_cipher).decode("utf-8"))
+    else:
+        decrypted_message = rsa.decrypt(int.from_bytes(extr_cipher, "big"),
+                                        server_keys["private_key"]).to_bytes(256, "big").lstrip(b'\x00')
+        
+        print(decrypted_message.decode("utf-8"))
 
     conn.close()
     s.close()
 
 
-def client(message_path, host="127.0.0.1", port=5000):
+def client(message_path, algorithm, host="127.0.0.1", port=5000):
     if message_path:
         try:
             with open(message_path, "rb") as f:
@@ -80,6 +93,10 @@ def client(message_path, host="127.0.0.1", port=5000):
     else:
         message = input("Message to send:").encode("utf-8") 
 
+    if algorithm == "rsa" and len(message) > 256:
+        print("Enter less than 256 bytes for RSA")
+        return
+
     client_keys = rsa.generate_keys(2048)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -92,19 +109,33 @@ def client(message_path, host="127.0.0.1", port=5000):
     server_n = int.from_bytes(server_raw, "big")
     server_pubkey = (65537, server_n)
 
-    chacha_key = secrets.token_bytes(32)
-    chacha_nonce = secrets.token_bytes(12)
+    if algorithm == "chacha20":
+        chacha_key = secrets.token_bytes(32)
+        chacha_nonce = secrets.token_bytes(12)
 
-    cipher = cha.encrypt(chacha_key, chacha_nonce, message)
-    chacha_key_nonce =  chacha_nonce + chacha_key
+        cipher = cha.encrypt(chacha_key, chacha_nonce, message)
+        chacha_key_nonce =  chacha_nonce + chacha_key
 
-    encrypted_key_nonce = rsa.encrypt(int.from_bytes(chacha_key_nonce, "big"), 
-                                      server_pubkey).to_bytes(256, "big")
+        encrypted_key_nonce = rsa.encrypt(int.from_bytes(chacha_key_nonce, "big"), 
+                                          server_pubkey).to_bytes(256, "big")
+        
+    else:
+        padded_message = message.rjust(256, b'\x00')
+        cipher = rsa.encrypt(int.from_bytes(padded_message, "big"),
+                             server_pubkey).to_bytes(256, "big")
+        encrypted_key_nonce = b""
+
+
+    hash = hashlib.sha256(cipher).digest()
+    signature = rsa.encrypt(int.from_bytes(hash, "big"),
+                            client_keys["private_key"]).to_bytes(256, "big")
 
     pdf_path = "./sample.pdf"
     create_pdf(pdf_path)
 
     payload = {
+        "algorithm": algorithm,
+        "signature": base64.b64encode(signature).decode("ascii"),
         "ciphertext": base64.b64encode(cipher).decode("ascii"),
         "encrypted_key_nonce": base64.b64encode(encrypted_key_nonce).decode("ascii"),
     }
